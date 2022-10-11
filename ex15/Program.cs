bdeedc273012fc76e9060c62272ceb7584f81502
Ekaterina Shvalyuk <ekaterinashvalyuk@AirdiEkaterina.lan>
2022-10-11 17:27:19 +0200

    ex15new

--- ex15/Program.cs
+++ ex15/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+Console.Write("enter a number rappresenting a day of the week: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num<=5)
+{
+    Console.Write($"{num}-> no");
+}
+else if (num==6 || num ==7)
+{
+    Console.Write($"{num} -> yes");
+}
+else if (num>=8)
+{
+    Console.WriteLine("theres no such day of the week");
+}
